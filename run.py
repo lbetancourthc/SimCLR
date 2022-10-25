96387f550e045f46fa181e9a8c3a6d231f7ea044
Landneyker <82390922+lbetancourthc@users.noreply.github.com>
2022-10-25 11:12:40 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import argparse
+import pandas as pd
 import torch
 import torch.backends.cudnn as cudnn
+
 from torchvision import models
-from data_aug.contrastive_learning_dataset import ContrastiveLearningDataset
+from data_aug.contrastive_learning_dataset import ContrastiveLearningDataset, PANDA_Dataset
 from models.resnet_simclr import ResNetSimCLR
 from simclr import SimCLR
 
@@ -11,46 +13,32 @@ model_names = sorted(name for name in models.__dict__
                      and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch SimCLR')
-parser.add_argument('-data', metavar='DIR', default='./datasets',
-                    help='path to dataset')
-parser.add_argument('-dataset-name', default='stl10',
-                    help='dataset name', choices=['stl10', 'cifar10'])
-parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
-                    choices=model_names,
-                    help='model architecture: ' +
-                         ' | '.join(model_names) +
-                         ' (default: resnet50)')
-parser.add_argument('-j', '--workers', default=12, type=int, metavar='N',
-                    help='number of data loading workers (default: 32)')
-parser.add_argument('--epochs', default=200, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('-b', '--batch-size', default=256, type=int,
-                    metavar='N',
-                    help='mini-batch size (default: 256), this is the total '
-                         'batch size of all GPUs on the current node when '
-                         'using Data Parallel or Distributed Data Parallel')
-parser.add_argument('--lr', '--learning-rate', default=0.0003, type=float,
-                    metavar='LR', help='initial learning rate', dest='lr')
-parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)',
-                    dest='weight_decay')
-parser.add_argument('--seed', default=None, type=int,
-                    help='seed for initializing training. ')
-parser.add_argument('--disable-cuda', action='store_true',
-                    help='Disable CUDA')
-parser.add_argument('--fp16-precision', action='store_true',
-                    help='Whether or not to use 16-bit precision GPU training.')
-
-parser.add_argument('--out_dim', default=128, type=int,
-                    help='feature dimension (default: 128)')
-parser.add_argument('--log-every-n-steps', default=100, type=int,
-                    help='Log every n steps')
-parser.add_argument('--temperature', default=0.07, type=float,
-                    help='softmax temperature (default: 0.07)')
-parser.add_argument('--n-views', default=2, type=int, metavar='N',
-                    help='Number of views for contrastive learning training.')
+parser.add_argument('-data', metavar='DIR', default='./datasets', help='path to dataset')
+parser.add_argument('-dataset-name', default='stl10', help='dataset name', choices=['stl10', 'cifar10'])
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18', choices=model_names, help='model architecture: ' + ' | '.join(model_names) + ' (default: resnet50)')
+parser.add_argument('-j', '--workers', default=12, type=int, metavar='N', help='number of data loading workers (default: 32)')
+parser.add_argument('--epochs', default=200, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('-b', '--batch-size', default=256, type=int, metavar='N', help='mini-batch size (default: 256), this is the total batch size of all GPUs on the current node when using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=0.0003, type=float, metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float, metavar='W', help='weight decay (default: 1e-4)', dest='weight_decay')
+parser.add_argument('--seed', default=None, type=int, help='seed for initializing training. ')
+parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
+parser.add_argument('--fp16-precision', action='store_true', help='Whether or not to use 16-bit precision GPU training.')
+parser.add_argument('--out_dim', default=128, type=int, help='feature dimension (default: 128)')
+parser.add_argument('--log-every-n-steps', default=100, type=int, help='Log every n steps')
+parser.add_argument('--temperature', default=0.07, type=float, help='softmax temperature (default: 0.07)')
+parser.add_argument('--n-views', default=2, type=int, metavar='N', help='Number of views for contrastive learning training.')
 parser.add_argument('--gpu-index', default=0, type=int, help='Gpu index.')
 
+
+main_dir = '/content/drive/Shareddrives/Digital Pathology/PANDA'
+tiles_dir = '/content/patches'
+model_dir = os.path.join(main_dir, 'Model')
+train_csv = os.path.join(main_dir, 'df_train')
+test_csv = os.path.join(main_dir, 'df_test')
+val_csv = os.path.join(main_dir, 'df_val')
+tiles_csv = os.path.join(main_dir, 'tiles.csv')
+tiles_zip = os.path.join(main_dir, 'tiles.zip')
 
 def main():
     args = parser.parse_args()
@@ -64,13 +52,21 @@ def main():
         args.device = torch.device('cpu')
         args.gpu_index = -1
 
-    dataset = ContrastiveLearningDataset(args.data)
+    #dataset = ContrastiveLearningDataset(args.data)
+    #train_dataset = dataset.get_dataset(args.dataset_name, args.n_views)
+    df_train = pd.read_csv(train_csv)
+    train_dataset = PANDA_Dataset(tiles_dir, df_train)
 
-    train_dataset = dataset.get_dataset(args.dataset_name, args.n_views)
-
-    train_loader = torch.utils.data.DataLoader(
+    """train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=args.batch_size, shuffle=True,
-        num_workers=args.workers, pin_memory=True, drop_last=True)
+        num_workers=args.workers, pin_memory=True, drop_last=True)"""
+
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=args.batch_size,
+                                               shuffle=True,
+                                               num_workers=args.workers,
+                                               pin_memory=True,
+                                               drop_last=True)
 
     model = ResNetSimCLR(base_model=args.arch, out_dim=args.out_dim)
 
